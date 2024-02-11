827591da44d21db46ba0e3993cdf3ce4098522ce
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-02-11 21:46:33 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# kwaa/nixos [WIP]
+
+```bash
+cd /tmp
+
+curl https://raw.githubusercontent.com/kwaa/nixos/main/disko-config.nix -o /tmp/disko-config.nix
+```
+
+## Useful links
+
+- https://github.com/nix-community/disko/blob/master/docs/quickstart.md
---
+++ disko-config.nix
@@ -0,0 +1,60 @@
+{
+  disko.devices = {
+    disk = {
+      nvme0n1 = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                # disable settings.keyFile if you want to use interactive password entry
+                settings.allowDiscards = true;
+                passwordFile = "/tmp/secret.key"; # Interactive
+                # settings = {
+                #   allowDiscards = true;
+                #   keyFile = "/tmp/secret.key";
+                # };
+                # additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
