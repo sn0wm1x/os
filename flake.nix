99bf3d1b69c6d215fb43fe25f82379aebf9e9c97
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-02-16 22:18:23 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -13,6 +13,9 @@
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
 
+    # https://github.com/nix-community/impermanence#nixos
+    impermanence.url = "github:nix-community/impermanence";
+
     # https://nix-community.github.io/home-manager/#ch-nix-flakes
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -25,6 +28,7 @@
       modules = [
         ./configuration.nix
         disko.nixosModules.disko
+        impermanence.nixosModules.impermanence
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
