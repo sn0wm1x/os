fe0afd18c125ba5f6c969a2b4d29f9072a6680bc
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-02-16 22:18:13 +0800

    Update disko-config.nix

--- disko-config.nix
+++ disko-config.nix
@@ -19,13 +19,15 @@
                 ];
               };
             };
-            system = {
+            luks = {
               size = "100%";
               content = {
                 type = "luks";
-                name = "system";
-                # disable settings.keyFile if you want to use interactive password entry
+                name = "cryptroot";
                 settings.allowDiscards = true;
+                settings.bypassWorkqueues = true;
+                settings.crypttabExtraOpts = [ "fido2-device=auto" "token-timeout=10" ];
+                # disable settings.keyFile if you want to use interactive password entry
                 passwordFile = "/tmp/secret.key"; # Interactive
                 # settings = {
                 #   allowDiscards = true;
@@ -36,17 +38,21 @@
                   type = "btrfs";
                   extraArgs = [ "-f" ];
                   subvolumes = {
-                    "/root" = {
-                      mountpoint = "/";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/home" = {
-                      mountpoint = "/home";
-                      mountOptions = [ "compress=zstd" "noatime" ];
+                    # "/root" = {
+                    #   mountpoint = "/";
+                    #   mountOptions = [ "compress-force=zstd:1" "noatime" ];
+                    # };
+                    # "/home" = {
+                    #   mountpoint = "/home";
+                    #   mountOptions = [ "compress-force=zstd:1" "noatime" ];
+                    # };
+                    "/persist" = {
+                      mountpoint = "/persist";
+                      mountOptions = [ "compress-force=zstd:1" "noatime" ];
                     };
                     "/nix" = {
                       mountpoint = "/nix";
-                      mountOptions = [ "compress=zstd" "noatime" ];
+                      mountOptions = [ "compress-force=zstd:1" "noatime" ];
                     };
                   };
                 };
@@ -56,5 +62,13 @@
         };
       };
     };
+    nodev = {
+      "/" = {
+        fsType = "tmpfs";
+        mountOptions = [ "defaults" "size=4G" "mode=755" ];
+      };
+    };
   };
+
+  fileSystems."/persist".neededForBoot = true;
 }
