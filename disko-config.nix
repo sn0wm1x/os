b9950da9b94da6d42658ae48bb4a344b18452ea6
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-02-28 00:08:33 +0800

    Update disko-config.nix

--- disko-config.nix
+++ disko-config.nix
@@ -1,57 +1,57 @@
 {
   disko.devices = {
-    disk = {
-      nvme0n1 = {
-        type = "disk";
-        device = "/dev/nvme0n1";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              size = "1G";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [
-                  "defaults"
-                ];
-              };
+    disk.nvme0n1 = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "defaults" ];
             };
-            luks = {
-              size = "100%";
+          };
+          luks = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "cryptroot";
+              settings = {
+                allowDiscards = true;
+                bypassWorkqueues = true;
+                keyFile = "/key/nixos.key";
+                keyFileSize = 8192;
+                preOpenCommands = ''
+                  mkdir -m 0755 -p /key
+                  sleep 2
+                  mount -n -t vfat -o ro /dev/disk/by-id/${PRIMARYUSBID} /key || mount -n -t vfat -o ro /dev/disk/by-id/${BACKUPUSBID} /key
+                ''
+                postOpenCommands = ''
+                  umount /key
+                  rm -rf /key
+                ''
+                # sleep 5
+                # until mount -n -t vfat -o ro /dev/disk/by-id/${PRIMARYUSBID} /key 2>&1 1>/dev/null; do
+                #   echo 'Could not find /dev/disk/by-id/${PRIMARYUSBID}, retrying...'
+                #   sleep 2
+                # done
+              };
               content = {
-                type = "luks";
-                name = "cryptroot";
-                settings.allowDiscards = true;
-                settings.bypassWorkqueues = true;
-                settings.crypttabExtraOpts = [ "fido2-device=auto" "token-timeout=10" ];
-                # disable settings.keyFile if you want to use interactive password entry
-                # settings.keyFile = "/tmp/secret.key";
-                # additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
-                # systemd-cryptenroll --recovery-key
-                passwordFile = "/tmp/secret.key"; # Interactive
-                content = {
-                  type = "btrfs";
-                  extraArgs = [ "-f" ];
-                  subvolumes = {
-                    # "/root" = {
-                    #   mountpoint = "/";
-                    #   mountOptions = [ "compress-force=zstd:1" "noatime" ];
-                    # };
-                    # "/home" = {
-                    #   mountpoint = "/home";
-                    #   mountOptions = [ "compress-force=zstd:1" "noatime" ];
-                    # };
-                    "/persist" = {
-                      mountpoint = "/persist";
-                      mountOptions = [ "compress-force=zstd:1" "noatime" ];
-                    };
-                    "/nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = [ "compress-force=zstd:1" "noatime" ];
-                    };
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "/persist" = {
+                    mountpoint = "/persist";
+                    mountOptions = [ "compress-force=zstd:1" "noatime" ];
+                  };
+                  "/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [ "compress-force=zstd:1" "noatime" ];
                   };
                 };
               };
@@ -60,13 +60,10 @@
         };
       };
     };
-    nodev = {
-      "/" = {
-        fsType = "tmpfs";
-        mountOptions = [ "defaults" "size=4G" "mode=755" ];
-      };
+    nodev."/" = {
+      fsType = "tmpfs";
+      mountOptions = [ "defaults" "size=4G" "mode=755" "noatime" ];
     };
   };
-
   fileSystems."/persist".neededForBoot = true;
 }
