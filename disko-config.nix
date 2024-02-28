79bdd7dd021cff9a982a5403af5246451e1b4c61
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-02-28 14:12:08 +0800

    Update disko-config.nix

--- disko-config.nix
+++ disko-config.nix
@@ -26,20 +26,21 @@
                 bypassWorkqueues = true;
                 keyFile = "/key/nixos.key";
                 keyFileSize = 8192;
+                # https://nixos.wiki/wiki/Full_Disk_Encryption#Option_2:_Copy_Key_as_file_onto_a_vfat_usb_stick
+                # https://github.com/reo101/rix101/blob/a6efd4146bbe0c7fb44343225b9dbf9585472597/machines/nixos/x86_64-linux/jeeves/disko.nix#L94-L107
                 preOpenCommands = ''
                   mkdir -m 0755 -p /key
                   sleep 2
-                  mount -n -t vfat -o ro /dev/disk/by-id/${PRIMARYUSBID} /key || mount -n -t vfat -o ro /dev/disk/by-id/${BACKUPUSBID} /key
-                ''
+                  until mount -n -t vfat -o ro /dev/disk/by-id/${USBID} /key 2>&1 1>/dev/null; do
+                    echo 'Could not find /dev/disk/by-id/${USBID}, retrying...'
+                    sleep 2
+                  done
+                '';
                 postOpenCommands = ''
                   umount /key
                   rm -rf /key
-                ''
-                # sleep 5
-                # until mount -n -t vfat -o ro /dev/disk/by-id/${PRIMARYUSBID} /key 2>&1 1>/dev/null; do
-                #   echo 'Could not find /dev/disk/by-id/${PRIMARYUSBID}, retrying...'
-                #   sleep 2
-                # done
+                '';
+                # mount -n -t vfat -o ro /dev/disk/by-id/${PRIMARYUSBID} /key || mount -n -t vfat -o ro /dev/disk/by-id/${BACKUPUSBID} /key
               };
               content = {
                 type = "btrfs";
@@ -60,9 +61,15 @@
         };
       };
     };
-    nodev."/" = {
-      fsType = "tmpfs";
-      mountOptions = [ "defaults" "size=4G" "mode=755" "noatime" ];
+    nodev = {
+      "/" = {
+        fsType = "tmpfs";
+        mountOptions = [ "defaults" "size=2G" "mode=755" "noatime" ];
+      };
+      "/home/kwa" = {
+        fsType = "tmpfs";
+        mountOptions = [ "defaults" "size=2G" "mode=777" "noatime" ];
+      }
     };
   };
   fileSystems."/persist".neededForBoot = true;
