c332d4394d64f0a5db1a0e421167e27a9ae28c56
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-01 21:35:19 +0800

    disable limit

--- hosts/bluestar/default.nix
+++ hosts/bluestar/default.nix
@@ -10,7 +10,7 @@
   ];
 
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
+  # boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "bluestar";
