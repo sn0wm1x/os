787b5847de1557e57f3604d78762bca1da2e4045
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-01 13:51:18 +0800

    Update git.nix

--- home/shared/features/cli/git.nix
+++ home/shared/features/cli/git.nix
@@ -1,6 +1,12 @@
 {
   # git
   programs.git.enable = true;
+  programs.git.userName = "藍+85CD";
+  programs.git.userEmail = "50108258+kwaa@users.noreply.github.com";
+  programs.git.extraConfig = {
+    init.defaultBranch = "main";
+    # safe.directory = [];
+  };
 
   # gh
   programs.gh.enable = true;
