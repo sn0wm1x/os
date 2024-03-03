c96f3b58e20eea2810a776bd622f62218324293e
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-03 14:32:22 +0800

    setup direnv

---
+++ home/shared/features/cli/direnv.nix
@@ -0,0 +1,6 @@
+{
+  # https://github.com/nix-community/nix-direnv#via-home-manager
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
+  programs.direnv.enableNushellIntegration = true;
+}
