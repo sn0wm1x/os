b706b470e2aa27c9250079c1e43c3f38742aec65
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-03 20:38:04 +0800

    use latest xanmod kernel

--- hosts/bluestar/hardware.nix
+++ hosts/bluestar/hardware.nix
@@ -7,22 +7,21 @@
 
   # boot.initrd.systemd.enable = true;
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "uas" "sd_mod" ];
-  # boot.initrd.kernelModules = [ "uas" "usbcore" "usb_storage" "vfat" "nls_cp437" "nls_iso8859_1" ];
-  boot.initrd.kernelModules = [ "i915" "uas" "usbcore" "usb_storage" "vfat" "nls_cp437" "nls_iso8859_1" ];
+  boot.initrd.kernelModules = [ "uas" "usbcore" "usb_storage" "vfat" "nls_cp437" "nls_iso8859_1" ];
   boot.kernelModules = [ "kvm-intel" ];
-  boot.kernelPackages = pkgs.linuxPackages_6_1;
+  # boot.kernelPackages = pkgs.linuxPackages_6_1;
   # boot.kernelPackages = pkgs.linuxPackages_latest;
-  # boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
 
   # https://nixos.wiki/wiki/Intel_Graphics
   # Intel Corporation DG2 [Arc A770] [8086:56a0]
-  boot.kernelParams = [
+  # boot.kernelParams = [
     # disable GuC, enable HuC
     # https://wiki.archlinux.org/title/intel_graphics#Enable_GuC_/_HuC_firmware_loading
     # "i915.enable_guc=2"
     # force use A770
     # "i915.force_probe=56a0"
-  ];
+  # ];
 
   services.xserver.videoDrivers = [ "intel" ];
 
