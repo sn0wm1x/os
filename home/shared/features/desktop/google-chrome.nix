569c681ac35ecacc02fa944371187d0f3f0fab23
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-03 15:03:14 +0800

    chore(google-chrome): update command line args

--- home/shared/features/desktop/google-chrome.nix
+++ home/shared/features/desktop/google-chrome.nix
@@ -4,7 +4,8 @@
     (google-chrome.override {
       # fcitx5 wayland fix
       # https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland#Chromium_.2F_Electron
-      commandLineArgs = "--gtk-version=4";
+      # commandLineArgs = "--gtk-version=4";
+      commandLineArgs = "--enable-wayland-ime";
     })
   ];
   home.persistence."/persist/home/kwa".directories = [
