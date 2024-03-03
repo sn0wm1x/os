e5d32cfb27de10689fb1e83630b5b9a0b8c7a1ce
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-03 17:16:22 +0800

    fill hash

--- home/kwa/programs/gpg.nix
+++ home/kwa/programs/gpg.nix
@@ -4,7 +4,7 @@
     source = pkgs.fetchurl {
       # https://keys.openpgp.org/search?q=4444777733334444
       url = "https://keys.openpgp.org/vks/v1/by-fingerprint/ABCBA12F1A8E3CCCF10B51094444777733334444";
-      hash = "";
+      hash = "oJPK2KWqbM5K9wSLR5m2M8zLWe8HJYL5qrXVpAJBFKk=";
     };
     trust = 5;
   }];
