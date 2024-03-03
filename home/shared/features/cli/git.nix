3d32b58f30fdb3ac83e0d58dda779a82b62231ac
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-03 14:19:12 +0800

    enable git lfs

--- home/shared/features/cli/git.nix
+++ home/shared/features/cli/git.nix
@@ -1,6 +1,7 @@
 {
   # git
   programs.git.enable = true;
+  programs.git.lfs.enable = true;
   programs.git.userName = "藍+85CD";
   programs.git.userEmail = "50108258+kwaa@users.noreply.github.com";
   programs.git.extraConfig = {
