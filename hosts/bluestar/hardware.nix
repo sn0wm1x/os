0037e019df66ca52b88f68c4591ac5e0d359041e
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-03 18:57:13 +0800

    try a770

--- hosts/bluestar/hardware.nix
+++ hosts/bluestar/hardware.nix
@@ -9,12 +9,13 @@
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "uas" "sd_mod" ];
   boot.initrd.kernelModules = [ "uas" "usbcore" "usb_storage" "vfat" "nls_cp437" "nls_iso8859_1" ];
   boot.kernelModules = [ "kvm-intel" ];
-  boot.kernelPackages = pkgs.linuxPackages_6_1;
+  # boot.kernelPackages = pkgs.linuxPackages_6_1;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   # boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
 
   # https://nixos.wiki/wiki/Intel_Graphics
   # Intel Corporation DG2 [Arc A770] [8086:56a0]
-  # boot.kernelParams = [ "i915.force_probe=56a0" ];
+  boot.kernelParams = [ "i915.force_probe=56a0" ];
 
   services.xserver.videoDrivers = [ "intel" ];
 
