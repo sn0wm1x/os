7c18218b3d17af1caf88e0144d94bee69af30105
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-05 19:14:56 +0800

    update ulimit

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -13,6 +13,22 @@
     thefuck
   ];
 
+   # Increase open file limit for sudoers
+  security.pam.loginLimits = [
+    {
+      domain = "@wheel";
+      item = "nofile";
+      type = "soft";
+      value = "524288";
+    }
+    {
+      domain = "@wheel";
+      item = "nofile";
+      type = "hard";
+      value = "1048576";
+    }
+  ];
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = lib.mkDefault "23.11";
 }
