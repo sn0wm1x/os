2ad352fd62a6d8fa27aac547fe52cf01474f083e
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-13 00:10:02 +0800

    chore(blackbox-terminal): update font config

--- home/shared/features/desktop/blackbox-terminal/default.nix
+++ home/shared/features/desktop/blackbox-terminal/default.nix
@@ -11,7 +11,7 @@
   };
   dconf.settings = {
     "com/raggesilver/BlackBox" = {
-      font = "MonospiceNe Nerd Font 12";
+      font = "MonospiceNe Nerd Font Mono 12";
       theme-bold-is-bright = true;
       theme-dark = "Catppuccin-Frappe";
       theme-light = "Catppuccin-Latte";
