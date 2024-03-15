432c5ab18dfdd3e95def3b25f982120c88827f5a
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-16 00:25:49 +0800

    chore: add todo

--- hosts/shared/features/cli/misc.nix
+++ hosts/shared/features/cli/misc.nix
@@ -1,4 +1,5 @@
 { pkgs, ... }: {
+  # TODO: https://github.com/nix-community/nix-index-database
   environment.systemPackages = with pkgs; [
     comma # https://github.com/nix-community/comma
   ];
