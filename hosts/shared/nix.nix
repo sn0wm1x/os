0478eb0475f634b26ef218057057d9ae3be89273
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-16 22:28:56 +0800

    chore: update nix config

--- hosts/shared/nix.nix
+++ hosts/shared/nix.nix
@@ -1,9 +1,14 @@
 { config, inputs, lib, ... }: {
+  nixpkgs.config.allowUnfree = true;
+
   nix.settings.experimental-features = "nix-command flakes";
   nix.settings.auto-optimise-store = true;
+  nix.channel.enable = false;
+
   nix.gc.automatic = true;
   nix.gc.dates = "weekly";
   nix.gc.options = "--delete-older-than 1w";
+  
   # This will add each flake input as a registry
   # To make nix3 commands consistent with your flake
   nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
@@ -17,24 +22,4 @@
       value.source = value.flake;
     })
     config.nix.registry;
-
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-    };
-  };
 }
