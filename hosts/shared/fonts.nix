d9b2d3c0f9215608fb765ff469d53e701c47ad35
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-16 22:20:07 +0800

    chore: update fonts

--- hosts/shared/fonts.nix
+++ hosts/shared/fonts.nix
@@ -3,26 +3,37 @@
   fonts.fontDir.enable = true;
   fonts.packages = with pkgs; [
     noto-fonts
+    # noto-fonts-extra
     # noto-fonts-cjk
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
-    noto-fonts-color-emoji
-    noto-fonts-emoji
-    # monaspace
+    # monospace
+    monaspace
     (nerdfonts.override { fonts = [
+      "NerdFontsSymbolsOnly"
       "Monaspace"
     ]; })
+    # emoji / icon
+    noto-fonts-color-emoji
+    noto-fonts-emoji
+    material-symbols
+    material-design-icons
+    font-awesome
   ];
   fonts.fontconfig.defaultFonts = {
     sansSerif = [
       "Noto Sans"
       "Noto Sans CJK SC"
+      "Noto Sans CJK TC"
+      "Noto Sans CJK JP"
       "Noto Color Emoji"
       "Noto Emoji"
     ];
     serif = [
       "Noto Serif"
       "Noto Serif CJK SC"
+      "Noto Serif CJK TC"
+      "Noto Serif CJK JP"
       "Noto Color Emoji"
       "Noto Emoji"
     ];
@@ -30,6 +41,12 @@
       "MonaspiceNe Nerd Font Mono"
       "Noto Sans Mono"
       "Noto Sans Mono CJK SC"
+      "Noto Sans Mono CJK TC"
+      "Noto Sans Mono CJK JP"
+      "Noto Color Emoji"
+      "Noto Emoji"
+    ];
+    emoji = [
       "Noto Color Emoji"
       "Noto Emoji"
     ];
