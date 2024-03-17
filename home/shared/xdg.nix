6d0dfde0934828a4263361b0b1f98406f7c6f015
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-17 19:52:03 +0800

    chore: update xdg config

--- home/shared/xdg.nix
+++ home/shared/xdg.nix
@@ -8,6 +8,7 @@
   # mime
   xdg.mime.enable = true;
   xdg.mimeApps.enable = true;
+  xdg.configFile."mimeapps.list".force = true;
 
   # user-dirs
   xdg.configFile."user-dirs.locale".text = lib.mkForce "en_US";
@@ -20,4 +21,8 @@
   xdg.userDirs.download = "${config.home.homeDirectory}/Downloads";
   xdg.userDirs.templates = "${config.home.homeDirectory}/Templates";
   xdg.userDirs.documents = "${config.home.homeDirectory}/Documents";
+  xdg.userDirs.publicShare = "${config.home.homeDirectory}/Public";
+  xdg.userDirs.extraConfig = {
+    XDG_SCREENSHOTS_DIR = "${config.xdg.userDirs.pictures}/Screenshots";
+  };
 }
