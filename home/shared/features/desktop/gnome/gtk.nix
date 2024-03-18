af68b2beb2cc4d2158c4664d83fa25dbc2885e5f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-18 16:45:00 +0800

    chore(gtk): set icon theme

--- home/shared/features/desktop/gnome/gtk.nix
+++ home/shared/features/desktop/gnome/gtk.nix
@@ -1,16 +1,22 @@
-{ pkgs, ... }: {
-  gtk =
-    let extraConfig = {
+{pkgs, ...}: {
+  gtk = let
+    extraConfig = {
       gtk-application-prefer-dark-theme = 1;
       gtk-error-bell = 0;
     };
-    in {
-      enable = true;
+  in {
+    enable = true;
 
-      theme.name = "adw-gtk3";
-      theme.package = pkgs.adw-gtk3;
+    theme.name = "adw-gtk3";
+    theme.package = pkgs.adw-gtk3;
 
-      gtk3 = { inherit extraConfig; };
-      gtk4 = { inherit extraConfig; };
+    iconTheme.name = "Papirus";
+    iconTheme.package = pkgs.catppuccin-papirus-folders.override {
+      accent = "blue";
+      flavor = "frappe";
     };
+
+    gtk3 = {inherit extraConfig;};
+    gtk4 = {inherit extraConfig;};
+  };
 }
