bf7e3174d40941b403210e0f877acdd4e2023aa6
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-18 14:28:54 +0800

    chore(gnome): exclude packages

--- hosts/shared/features/desktop/gnome/default.nix
+++ hosts/shared/features/desktop/gnome/default.nix
@@ -1,27 +1,53 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.desktopManager.xterm.enable = false;
+  services.xserver.excludePackages = with pkgs; [xterm];
 
-  environment.systemPackages = with pkgs; [
-    gnome.dconf-editor
-    gnome.gnome-shell-extensions
-    gnome.gnome-tweaks
-    # 功能
-    gnomeExtensions.vitals
-    gnomeExtensions.kimpanel
-    gnomeExtensions.gsconnect
-    #
-    gnomeExtensions.paperwm
-    gnomeExtensions.dash-to-dock
-    gnomeExtensions.quick-settings-tweaker
-    # 外观
-    gnomeExtensions.user-themes
-    gnomeExtensions.blur-my-shell
-    gnomeExtensions.just-perfection
-    gnomeExtensions.rounded-window-corners
-    gnomeExtensions.gnome-40-ui-improvements
-  ];
+  # https://nixos.wiki/wiki/GNOME#Excluding_some_GNOME_applications_from_the_default_install
+  environment.gnome.excludePackages =
+    (with pkgs; [
+      # gnome-photos
+      gnome-tour
+    ])
+    ++ (with pkgs.gnome; [
+      cheese # webcam tool
+      gnome-music
+      # gnome-terminal
+      # gedit # text editor
+      epiphany # web browser
+      geary # email reader
+      # evince # document viewer
+      # gnome-characters
+      totem # video player
+      # tali # poker game
+      # iagno # go game
+      # hitori # sudoku game
+      # atomix # puzzle game
+    ]);
+
+  environment.systemPackages =
+    (with pkgs.gnome; [
+      dconf-editor
+      gnome-shell-extensions
+      gnome-tweaks
+    ])
+    ++ (with pkgs.gnomeExtensions; [
+      # 功能
+      vitals
+      kimpanel
+      gsconnect
+      #
+      paperwm
+      dash-to-dock
+      quick-settings-tweaker
+      # 外观
+      user-themes
+      blur-my-shell
+      just-perfection
+      # rounded-window-corners
+      gnome-40-ui-improvements
+    ]);
 }
