2aa92a018f4251e1ca41abe3fc85848b301a5f7c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-19 18:00:43 +0800

    fix(nushell): use shell aliases

--- home/shared/features/cli/nushell.nix
+++ home/shared/features/cli/nushell.nix
@@ -1,32 +1,35 @@
-{
+{config, ...}: {
   # https://nixos.wiki/wiki/Nushell
-  programs.nushell.enable = true;
-  programs.nushell.extraConfig = ''
-    let carapace_completer = {|spans|
-      carapace $spans.0 nushell $spans | from json
-    }
-    $env.config = {
-      show_banner: false,
-      completions: {
-      case_sensitive: false # case-sensitive completions
-      quick: true    # set to false to prevent auto-selecting completions
-      partial: true    # set to false to prevent partial filling of the prompt
-      algorithm: "fuzzy"    # prefix or fuzzy
-      external: {
-        # set to false to prevent nushell looking into $env.PATH to find more suggestions
-        enable: true 
-        # set to lower can improve completion performance at the cost of omitting some options
-        max_results: 100 
-        completer: $carapace_completer # check 'carapace_completer' 
+  programs.nushell = {
+    inherit (config.home) shellAliases;
+    enable = true;
+    extraConfig = ''
+      let carapace_completer = {|spans|
+        carapace $spans.0 nushell $spans | from json
+      }
+      $env.config = {
+        show_banner: false,
+        completions: {
+        case_sensitive: false # case-sensitive completions
+        quick: true    # set to false to prevent auto-selecting completions
+        partial: true    # set to false to prevent partial filling of the prompt
+        algorithm: "fuzzy"    # prefix or fuzzy
+        external: {
+          # set to false to prevent nushell looking into $env.PATH to find more suggestions
+          enable: true
+          # set to lower can improve completion performance at the cost of omitting some options
+          max_results: 100
+          completer: $carapace_completer # check 'carapace_completer'
+          }
         }
       }
-    } 
-    $env.PATH = ($env.PATH | 
-      split row (char esep) |
-      prepend /home/myuser/.apps |
-      append /usr/bin/env
-    )
-  '';
+      $env.PATH = ($env.PATH |
+        split row (char esep) |
+        prepend /home/myuser/.apps |
+        append /usr/bin/env
+      )
+    '';
+  };
 
   programs.atuin.enable = true;
   programs.atuin.enableNushellIntegration = true;
@@ -59,7 +62,7 @@
   programs.starship.enable = true;
   programs.starship.settings = {
     add_newline = true;
-    character = { 
+    character = {
       success_symbol = "[➜](bold green)";
       error_symbol = "[➜](bold red)";
     };
