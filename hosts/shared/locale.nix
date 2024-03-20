101dbcd8f0717b4a96c1830b9f683c6ff49c5311
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-21 02:06:31 +0800

    fix(locale): clean config

--- hosts/shared/locale.nix
+++ hosts/shared/locale.nix
@@ -13,11 +13,11 @@
     ];
   };
   time.timeZone = lib.mkDefault "Asia/Taipei";
-  time.hardwareClockInLocalTime = lib.mkForce true;
-  # https://www.linuxfromscratch.org/lfs/view/9.0-systemd-rc1/chapter07/clock.html
-  environment.etc.adjtime.text = ''
-    0.0 0 0.0
-    0
-    LOCAL
-  '';
+  # time.hardwareClockInLocalTime = lib.mkForce true;
+  # # https://www.linuxfromscratch.org/lfs/view/9.0-systemd-rc1/chapter07/clock.html
+  # environment.etc.adjtime.text = ''
+  #   0.0 0 0.0
+  #   0
+  #   LOCAL
+  # '';
 }
