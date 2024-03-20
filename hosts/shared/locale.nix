51adb6d487a5a9d5b9153471d31ef4b2afb7c792
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-21 01:50:13 +0800

    fix(locale): set adjtime

--- hosts/shared/locale.nix
+++ hosts/shared/locale.nix
@@ -14,4 +14,10 @@
   };
   time.timeZone = lib.mkDefault "Asia/Taipei";
   time.hardwareClockInLocalTime = lib.mkForce true;
+  # https://www.linuxfromscratch.org/lfs/view/9.0-systemd-rc1/chapter07/clock.html
+  environment.etc.adjtime.text = ''
+    0.0 0 0.0
+    0
+    LOCAL
+  '';
 }
