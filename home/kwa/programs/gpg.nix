868743657eb99d1885a10430fc8caebecb236277
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-04 20:50:31 +0800

    chore(gpg): persist private keys

--- home/kwa/programs/gpg.nix
+++ home/kwa/programs/gpg.nix
@@ -1,14 +1,24 @@
-{ pkgs, ... }: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   programs.gpg.enable = true;
-  programs.gpg.publicKeys = [{
-    source = pkgs.fetchurl {
-      # https://keys.openpgp.org/search?q=4444777733334444
-      url = "https://keys.openpgp.org/vks/v1/by-fingerprint/ABCBA12F1A8E3CCCF10B51094444777733334444";
-      sha256 = "oJPK2KWqbM5K9wSLR5m2M8zLWe8HJYL5qrXVpAJBFKk=";
-    };
-    trust = 5;
-  }];
+  programs.gpg.publicKeys = [
+    {
+      source = pkgs.fetchurl {
+        # https://keys.openpgp.org/search?q=4444777733334444
+        url = "https://keys.openpgp.org/vks/v1/by-fingerprint/ABCBA12F1A8E3CCCF10B51094444777733334444";
+        sha256 = "oJPK2KWqbM5K9wSLR5m2M8zLWe8HJYL5qrXVpAJBFKk=";
+      };
+      trust = 5;
+    }
+  ];
 
   # https://support.yubico.com/hc/en-us/articles/4819584884124-Resolving-GPG-s-CCID-conflicts
   programs.gpg.scdaemonSettings.disable-ccid = true;
+
+  home.persistence."/persist${config.home.homeDirectory}".directories = [
+    ".gnupg/private-keys-v1.d"
+  ];
 }
