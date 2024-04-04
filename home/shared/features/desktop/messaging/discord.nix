d12640c7ec971230fef387e6ed46c1d4ccaa11d3
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-04 13:42:14 +0800

    refactor(discord): original name & icon

--- home/shared/features/desktop/messaging/discord.nix
+++ home/shared/features/desktop/messaging/discord.nix
@@ -1,5 +1,21 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   # https://nixos.wiki/wiki/Discord#Vesktop
-  home.packages = with pkgs; [ vesktop ];
-  home.persistence."/persist/home/kwa".directories = [ ".config/vesktop" ];
+  home.packages = with pkgs; [
+    (vesktop.overrideAttrs {
+      # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/ve/vesktop/package.nix#L150-L161
+      desktopItems = [
+        (makeDesktopItem {
+          name = "vesktop";
+          desktopName = "Discord"; # original desktop name
+          exec = "vesktop %U";
+          icon = "discord"; # original icon
+          startupWMClass = " Vesktop";
+          genericName = "Internet Messenger";
+          keywords = ["discord" "vencord" "electron" "chat"];
+          categories = ["Network" "InstantMessaging" "Chat"];
+        })
+      ];
+    })
+  ];
+  home.persistence."/persist/home/kwa".directories = [".config/vesktop"];
 }
