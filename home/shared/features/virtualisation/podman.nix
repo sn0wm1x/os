1ee62fdb373633ee40bb0d2599556340fc7cb684
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-04 23:27:01 +0800

    chore(podman): add pods config

--- home/shared/features/virtualisation/podman.nix
+++ home/shared/features/virtualisation/podman.nix
@@ -14,6 +14,16 @@
   home.shellAliases = {
     docker-compose = "podman-compose";
   };
+
+  xdg.configFile."pods/connections.json".text = builtins.toJSON {
+    "21950ebf-11f6-4f6a-bfe8-10ea6b174e9c" = {
+      uuid = "21950ebf-11f6-4f6a-bfe8-10ea6b174e9c";
+      name = "Podman";
+      url = "unix:///run/user/1000/podman/podman.sock";
+      rgb = null;
+    };
+  };
+
   home.persistence."/persist${config.home.homeDirectory}".directories = [
     ".local/share/containers/storage"
   ];
