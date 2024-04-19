1e84aa311a348089b97ba68476a1b8abba5a00b1
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-19 22:50:11 +0800

    chore: more gnome circle apps

--- home/shared/features/desktop/gnome/circle.nix
+++ home/shared/features/desktop/gnome/circle.nix
@@ -1,8 +1,12 @@
 {pkgs, ...}: {
   # GNOME Circle Apps
   # https://circle.gnome.org/
+  # https://github.com/NixOS/nixpkgs/issues/195280
   home.packages = with pkgs; [
     # collision
+    # commit # missing
     eyedropper
+    fragments
+    junction
   ];
 }
