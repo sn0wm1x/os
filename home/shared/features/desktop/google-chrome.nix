d9081399e287b96c7a4fd70296ad4c4dec204241
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-22 11:39:52 +0800

    fix(google-chrome): set ozone platform

--- home/shared/features/desktop/google-chrome.nix
+++ home/shared/features/desktop/google-chrome.nix
@@ -1,11 +1,11 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    google-chrome
-    # (google-chrome.override {
-    #   # fcitx5 wayland fix
-    #   # https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland#Chromium_.2F_Electron
-    #   commandLineArgs = "--gtk-version=4";
-    # })
+    # google-chrome
+    (google-chrome.override {
+      # force ozone wayland & fcitx5 wayland fix
+      # https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland#Chromium_.2F_Electron
+      commandLineArgs = "--ozone-platform=wayland --gtk-version=4";
+    })
   ];
   home.persistence."/persist/home/kwa".directories = [
     ".config/google-chrome"
