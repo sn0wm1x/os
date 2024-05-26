514bd9060d7350f68f28e1e527695668092ae44f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-05-26 21:29:25 +0800

    chore: gnome extension settings

--- home/shared/features/desktop/gnome/extensions.nix
+++ home/shared/features/desktop/gnome/extensions.nix
@@ -34,4 +34,21 @@
     # rounded-window-corners
     "gnomeExtensions.gnome-40-ui-improvements@AXP.com"
   ];
+
+  # Blur My Shell
+  dconf.settings."org/gnome/shell/extensions/blur-my-shell/dash-to-dock" = {
+    blur = true;
+    brightness = 0.6;
+    override-background = true;
+    pipeline = "pipeline_default_rounded";
+    sigma = 30;
+    static-blur = true;
+    style-dash-to-dock = 1;
+    unblur-in-overview = false;
+  };
+
+  # Dash to Dock
+  dconf.settings."org/gnome/shell/extensions/dash-to-dock" = {
+    intellihide-mode = "ALL_WINDOWS";
+  };
 }
