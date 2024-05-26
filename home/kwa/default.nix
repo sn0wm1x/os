afbdeb02e0f75858e6c7f3d46269c771f52cc163
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-05-26 20:58:14 +0800

    chore: set account avatar

--- home/kwa/default.nix
+++ home/kwa/default.nix
@@ -1,5 +1,9 @@
 # https://github.com/Misterio77/nix-starter-configs/blob/main/minimal/home-manager/home.nix
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     ../shared
     ../shared/features/audio
@@ -12,6 +16,14 @@
 
   home.username = "kwa";
   home.homeDirectory = "/home/kwa";
+
+  home.file.".face".source = pkgs.fetchurl {
+    # https://www.pixiv.net/artworks/100897044
+    url = "https://i.pximg.net/img-original/img/2022/08/31/19/32/36/100897044_p0.png";
+    hash = "sha256-xO7ISZ/tT7HC8tV3apSVRTJxEpp5Ai6m5JzPPZvuCIo=";
+    curlOptsList = ["-e" "https://www.pixiv.net/"];
+  };
+
   home.persistence."/persist/home/kwa" = {
     # https://github.com/nix-community/impermanence#home-manager
     directories = [
