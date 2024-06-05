73dd1f559b572c299a8f3fa3cad785e20d8f54de
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-06-05 20:49:13 +0800

    fix(google-chrome): use `enable-wayland-ime` flag

--- home/shared/features/desktop/misc/google-chrome.nix
+++ home/shared/features/desktop/misc/google-chrome.nix
@@ -4,7 +4,8 @@
     (google-chrome.override {
       # force ozone wayland & fcitx5 wayland fix
       # https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland#Chromium_.2F_Electron
-      commandLineArgs = "--ozone-platform=wayland --gtk-version=4";
+      commandLineArgs = "--ozone-platform=wayland --enable-wayland-ime";
+      # commandLineArgs = "--ozone-platform=wayland --gtk-version=4";
     })
   ];
   home.persistence."/persist${config.home.homeDirectory}".directories = [
