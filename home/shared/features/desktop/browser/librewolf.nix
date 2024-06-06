e424cdc72a2de877aa75c72f7419458a7e55bf38
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-06-06 17:01:32 +0800

    feat(librewolf): add profiles

--- home/shared/features/desktop/browser/librewolf.nix
+++ home/shared/features/desktop/browser/librewolf.nix
@@ -1,8 +1,55 @@
+{ pkgs, ... }:
+let
+  firefox-gnome-theme = pkgs.fetchFromGitHub {
+    owner = "rafaelmardojai";
+    repo = "firefox-gnome-theme";
+    rev = "v126";
+    hash = "sha256-jVbj2JD5GRkP8s3vKBtd9PDpftf3kjLR0ZA/ND/c2+Q=";
+  };
+  betterfox = pkgs.fetchFromGitHub {
+    owner = "yokoffing";
+    repo = "Betterfox";
+    rev = "126.0";
+    hash = "sha256-W0JUT3y55ro3yU23gynQSIu2/vDMVHX1TfexHj1Hv7Q=";
+  };
+in
 {
   # https://nixos.wiki/wiki/Librewolf
-  programs.librewolf.enable = true;
-  programs.librewolf.settings = {
-    # Enable WebGL
-    "webgl.disabled" = false;
+  # programs.librewolf.enable = true;
+  # programs.librewolf.settings = {
+  #   # Enable WebGL
+  #   "webgl.disabled" = false;
+  # };
+
+  programs.firefox = {
+    enable = true;
+    package = pkgs.wrapFirefox pkgs.librewolf-unwrapped {
+      inherit (pkgs.librewolf-unwrapped) extraPrefsFiles extraPoliciesFiles;
+      wmClass = "LibreWolf";
+      libName = "librewolf";
+    };
+
+    # https://github.com/nix-community/home-manager/pull/5195
+    # languagePacks = [ "zh-CN" ];
+
+    profiles.kwa = {
+      isDefault = true;
+      id = 0;
+      name = "藍";
+
+      search.default = "StartPage";
+      search.privateDefault = "StartPage";
+
+      # firefox-gnome-theme
+      userContent = builtins.readFile "${firefox-gnome-theme}/userContent.css";
+      userChrome = builtins.readFile "${firefox-gnome-theme}/userChrome.css";
+
+      extraConfig = builtins.concatStringsSep "\n" [
+        (builtins.readFile "${firefox-gnome-theme}/configuration/user.js")
+        (builtins.readFile "${betterfox}/Fastfox.js")
+        (builtins.readFile "${betterfox}/Peskyfox.js")
+        (builtins.readFile "${betterfox}/Smoothfox.js")
+      ];
+    };
   };
 }
