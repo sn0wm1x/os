9329ed6f7dcd2bde1129f68711b420319d07ff4a
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-06-13 20:23:11 +0800

    chore(akahitoha): update hardware config

--- hosts/akahitoha/hardware.nix
+++ hosts/akahitoha/hardware.nix
@@ -15,12 +15,27 @@
   ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usbhid" "uas" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
+  # https://nixos.wiki/wiki/AMD_GPU#Make_the_kernel_use_the_correct_driver_early
+  boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelModules = [ "kvm-amd" ];
   # use linux 6.10+ testing kernel
   boot.kernelPackages = pkgs.linuxPackages_testing;
 
-  # TODO: other hardware config
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  hardware.opengl.enable = true;
+  # https://nixos.wiki/wiki/AMD_GPU#Vulkan
+  hardware.opengl.driSupport = true;
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.extraPackages = with pkgs; [
+    # https://nixos.wiki/wiki/AMD_GPU#OpenCL
+    rocmPackages.clr.icd
+    # https://nixos.wiki/wiki/AMD_GPU#AMDVLK
+    amdvlk
+  ];
+  hardware.opengl.extraPackages32 = with pkgs; [
+    driversi686Linux.amdvlk
+  ];
 
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
