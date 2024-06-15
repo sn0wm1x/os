86e960912f917cceb3a5440e3d08d1143132522b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-06-15 23:25:42 +0800

    chore(gpg): update public key hash

--- home/kwa/programs/gpg.nix
+++ home/kwa/programs/gpg.nix
@@ -8,7 +8,7 @@
       source = pkgs.fetchurl {
         # https://keys.openpgp.org/search?q=4444777733334444
         url = "https://keys.openpgp.org/vks/v1/by-fingerprint/ABCBA12F1A8E3CCCF10B51094444777733334444";
-        hash = "sha256-oJPK2KWqbM5K9wSLR5m2M8zLWe8HJYL5qrXVpAJBFKk=";
+        hash = "sha256-iywF/mKcNd2sPNDCVFvvhLD6IBmCTSh8gVh9YJHsgoE=";
       };
       trust = 5;
     }
