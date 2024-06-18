0262657d32edca45e162c625b20c72da21ba880f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-06-18 16:44:36 +0800

    feat(akahitoha): add ideapad extension

---
+++ home/shared/akahitoha/desktop/gnome/extensions.nix
@@ -0,0 +1,8 @@
+{ pkgs, ... }: {
+  home.packages = with pkgs.gnomeExtensions; [
+    ideapad # Lenovo IdeaPad goodies for Gnome Shell
+  ];
+  dconf.settings."org/gnome/shell".enabled-extensions = [
+    "ideapad@laurento.frittella"
+  ];
+}
