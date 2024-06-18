5506c01619ae90447e64cf1086a9ae298ec15f0c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-06-18 16:39:31 +0800

    chore(telegram): add todo

--- home/shared/features/desktop/messaging/telegram.nix
+++ home/shared/features/desktop/messaging/telegram.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [
     telegram-desktop
+    # TODO: use https://github.com/paper-plane-developers/paper-plane
   ];
   home.persistence."/persist${config.home.homeDirectory}".directories = [
     ".local/share/TelegramDesktop"
