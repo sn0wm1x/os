09f68e9666af88cc9b569de886c02f251de6bfae
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-06-20 16:47:47 +0800

    chore(just): add aliases

--- home/shared/features/cli/default.nix
+++ home/shared/features/cli/default.nix
@@ -8,10 +8,10 @@
     ./fzf.nix
     ./git.nix
     ./helix.nix
+    ./just.nix
     ./zellij.nix
   ];
   home.packages = with pkgs; [
-    just
     # eza # Better ls
     ripgrep # Better grep
     fd # Better find
---
+++ home/shared/features/cli/just.nix
@@ -0,0 +1,7 @@
+{ pkgs, ... }: {
+  home.packages = with pkgs; [ just ];
+
+  home.shellAliases = {
+    j = "just";
+  };
+}
