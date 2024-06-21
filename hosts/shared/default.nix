e9116c019ab14fe4ca3fb4f9459484c44d81551c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-06-21 18:56:23 +0800

    chore: remove login limits

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -16,22 +16,6 @@
   # sudo nopasswd
   security.sudo.wheelNeedsPassword = false;
 
-  # Increase open file limit for sudoers
-  security.pam.loginLimits = [
-    {
-      domain = "@wheel";
-      item = "nofile";
-      type = "soft";
-      value = "524288";
-    }
-    {
-      domain = "@wheel";
-      item = "nofile";
-      type = "hard";
-      value = "1048576";
-    }
-  ];
-
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = lib.mkDefault "23.11";
 }
