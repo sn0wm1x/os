6ca4ca337f6df319a137a2b043dc31350b869d7a
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-06-23 13:48:38 +0800

    chore: remove antares

--- home/shared/features/desktop/misc/database.nix
+++ home/shared/features/desktop/misc/database.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    antares # https://github.com/antares-sql/antares
+    # antares # https://github.com/antares-sql/antares
     sqlitebrowser # https://github.com/sqlitebrowser/sqlitebrowser
   ];
 }
