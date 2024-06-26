0598dee94561534d1d667ecc2095e21ce03bd193
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-06-26 20:22:50 +0800

    fix: set library path

--- hosts/shared/nix-ld.nix
+++ hosts/shared/nix-ld.nix
@@ -1,4 +1,8 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: {
   programs.nix-ld.enable = true;
   programs.nix-ld.package = pkgs.nix-ld-rs;
+
+  environment.sessionVariables = {
+    LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.stdenv.cc.cc ];
+  };
 }
