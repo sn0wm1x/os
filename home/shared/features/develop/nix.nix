da33eecebd75248eb9942f05e1eb5e374fddff17
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-07-05 00:20:42 +0800

    feat(nix): more packages

--- home/shared/features/develop/nix.nix
+++ home/shared/features/develop/nix.nix
@@ -1,12 +1,23 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     nil # language server
+
+    nixfmt-rfc-style # experimental formatter
     nixpkgs-fmt # formatter
+    nixpkgs-lint-community # linter
+    nixpkgs-hammering # fix PR
+    nixpkgs-review # review PR
+
+    nix-update # update nix packages
+    nix-inspect # inspect nix expressions
+    nix-output-monitor # `nom`, pretty print
 
     nurl # hash util
 
     # nvd # Differ
-    # nix-output-monitor
     # nh # Nice wrapper for NixOS and HM
   ];
+
+  # TODO: nix-index / nix-index-database
+  # https://github.com/nix-community/nix-index
 }
