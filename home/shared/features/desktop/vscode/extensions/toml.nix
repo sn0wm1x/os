7451b22fa435c362347b03d0e07a45ebd559d5b6
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-07-13 00:28:00 +0800

    fix(vscode): fix typo

--- home/shared/features/desktop/vscode/extensions/toml.nix
+++ home/shared/features/desktop/vscode/extensions/toml.nix
@@ -1,9 +1,8 @@
 { lib, pkgs, ... }:
 {
-  # programs.vscode.mutableExtensionsDir = false;
   programs.vscode = {
     extensions = with pkgs.vscode-extensions; [ tamasfe.even-better-toml ];
-    programs.vscode.userSettings = {
+    userSettings = {
       "evenBetterToml.taplo.bundled" = false;
       "evenBetterToml.taplo.path" = lib.getExe pkgs.taplo;
     };
