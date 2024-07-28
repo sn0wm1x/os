1ef8a1b8489165369baa46c92d1024bd34f36c99
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-07-28 15:19:33 +0800

    chore: add proxychains

--- hosts/shared/network.nix
+++ hosts/shared/network.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   # Firewall
   networking.firewall.enable = true;
 
@@ -8,4 +8,21 @@
   services.openssh.settings.PasswordAuthentication = false;
 
   # TODO: mosh
+
+  # Proxychains
+  programs.proxychains.enable = true;
+  programs.proxychains.package = pkgs.proxychains-ng;
+  programs.proxychains.quietMode = true;
+  programs.proxychains.proxies = {
+    naive = {
+      enable = true;
+      type = "socks5";
+      host = "127.0.0.1";
+      port = 10888;
+    };
+  };
+
+  environment.shellAliases = {
+    pc = "proxychains";
+  };
 }
