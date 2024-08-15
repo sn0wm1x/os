30bd49399af22b79b33ffa5115a16f1b43500556
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-15 12:12:51 +0800

    fix(gnome): remove text-input-v1 patch

--- overlays/gnome/default.nix
+++ overlays/gnome/default.nix
@@ -12,18 +12,18 @@ final: prev: {
         };
 
         # https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=mutter-text-input-v1
-        patches = [
-          # grab.patch
-          (prev.fetchpatch {
-            url = "https://gitlab.gnome.org/GNOME/mutter/-/commit/a99e139a68bd4e5350033163dfd85b6ce6da92a2.patch";
-            hash = "sha256-QyMRJ8QNxEj/WNfQM2TzZ5vq0xg33hlaE8UgjHM25Zs=";
-          })
-          # text-input-v1.patch
-          (prev.fetchpatch {
-            url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3751/diffs.patch";
-            hash = "sha256-KOpYon8Ajzn60voOAnj6wqXxZ7nnnJ9RIPhjGBKS5uY=";
-          })
-        ];
+        # patches = [
+        #   # grab.patch
+        #   (prev.fetchpatch {
+        #     url = "https://gitlab.gnome.org/GNOME/mutter/-/commit/a99e139a68bd4e5350033163dfd85b6ce6da92a2.patch";
+        #     hash = "sha256-QyMRJ8QNxEj/WNfQM2TzZ5vq0xg33hlaE8UgjHM25Zs=";
+        #   })
+        #   # text-input-v1.patch
+        #   (prev.fetchpatch {
+        #     url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3751/diffs.patch";
+        #     hash = "sha256-KOpYon8Ajzn60voOAnj6wqXxZ7nnnJ9RIPhjGBKS5uY=";
+        #   })
+        # ];
       });
     }
   );
