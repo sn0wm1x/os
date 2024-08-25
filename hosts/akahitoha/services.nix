9eeb35d02690b36ee19e31ef7d85fc678ad2ba38
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-25 17:03:34 +0800

    fix(ollama): update home

--- hosts/akahitoha/services.nix
+++ hosts/akahitoha/services.nix
@@ -5,6 +5,6 @@
     # https://github.com/ollama/ollama/pull/5426
     # https://github.com/ollama/ollama/pull/6282
     rocmOverrideGfx = "11.0.1";
-    home = "/home/kwa/Documents/ollama";
+    home = "/opt/ollama";
   };
 }
