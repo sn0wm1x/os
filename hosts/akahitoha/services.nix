7c610c55d9cf5c4c623c099c382e8508ed8809c3
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-25 16:23:36 +0800

    fix(ollama): enable

--- hosts/akahitoha/services.nix
+++ hosts/akahitoha/services.nix
@@ -1,6 +1,6 @@
 {
   services.ollama = {
-    enable = false;
+    enable = true;
     acceleration = "rocm";
     # https://github.com/ollama/ollama/pull/5426
     # https://github.com/ollama/ollama/pull/6282
