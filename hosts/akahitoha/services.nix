6ac9bbfd12180514538222e1e726107a70af2266
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-25 17:32:47 +0800

    fix(ollama): set home

--- hosts/akahitoha/services.nix
+++ hosts/akahitoha/services.nix
@@ -5,7 +5,7 @@
     # https://github.com/ollama/ollama/pull/5426
     # https://github.com/ollama/ollama/pull/6282
     rocmOverrideGfx = "11.0.1";
+    # sudo chown -R ollama:ollama /opt/ollama
+    home = "/opt/ollama";
   };
-
-  environment.persistence."/persist".directories = [ "/var/lib/ollama" ];
 }
