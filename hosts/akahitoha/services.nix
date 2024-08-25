5f0236c9691b7e9bae3a807b9f760ed4c873c3c0
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-25 17:16:23 +0800

    fix(ollama): set user

--- hosts/akahitoha/services.nix
+++ hosts/akahitoha/services.nix
@@ -5,6 +5,7 @@
     # https://github.com/ollama/ollama/pull/5426
     # https://github.com/ollama/ollama/pull/6282
     rocmOverrideGfx = "11.0.1";
-    home = "/opt/ollama";
+    user = "kwa";
+    home = "/home/kwa/Documents/ollama";
   };
 }
