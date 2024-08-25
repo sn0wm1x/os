fbc58538e9ef6fba6ce43ca85c4ed705b7d9652f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-25 16:03:33 +0800

    fix: save images to assets

--- home/kwa/default.nix
+++ home/kwa/default.nix
@@ -1,7 +1,6 @@
 # https://github.com/Misterio77/nix-starter-configs/blob/main/minimal/home-manager/home.nix
 { config
 , lib
-, pkgs
 , osConfig
 , ...
 }:
@@ -24,14 +23,15 @@ in
   home.username = "kwa";
   home.homeDirectory = "/home/kwa";
 
+  home.file.".face".source = ../../assets/100897044_p0.png;
   # home.file.".face".source = pkgs.fetchurl {
-    # https://www.pixiv.net/artworks/100897044
-    # url = "https://i.pximg.net/img-original/img/2022/08/31/19/32/36/100897044_p0.png";
-    # hash = "sha256-xO7ISZ/tT7HC8tV3apSVRTJxEpp5Ai6m5JzPPZvuCIo=";
-    # curlOptsList = [
-    #   "-e"
-    #   "https://www.pixiv.net/"
-    # ];
+  # https://www.pixiv.net/artworks/100897044
+  # url = "https://i.pximg.net/img-original/img/2022/08/31/19/32/36/100897044_p0.png";
+  # hash = "sha256-xO7ISZ/tT7HC8tV3apSVRTJxEpp5Ai6m5JzPPZvuCIo=";
+  # curlOptsList = [
+  #   "-e"
+  #   "https://www.pixiv.net/"
+  # ];
   # };
 
   home.persistence."/persist${config.home.homeDirectory}" = {
--- home/shared/features/desktop/gnome/wallpaper.nix
+++ home/shared/features/desktop/gnome/wallpaper.nix
@@ -1,14 +1,14 @@
-{ pkgs, ... }:
 let
-  wallpaper = pkgs.fetchurl {
-    # https://www.pixiv.net/artworks/103085198
-    url = "https://i.pximg.net/img-original/img/2022/11/25/18/08/43/103085198_p0.jpg";
-    hash = "sha256-BOy35x/9AGHCkY0fLTrwPTsyAvctwYUkrjDvzhLZzbM=";
-    curlOptsList = [
-      "-e"
-      "https://www.pixiv.net/"
-    ];
-  };
+  wallpaper = ../../../../../assets/103085198_p0.jpg;
+  # wallpaper = pkgs.fetchurl {
+  #   # https://www.pixiv.net/artworks/103085198
+  #   url = "https://i.pximg.net/img-original/img/2022/11/25/18/08/43/103085198_p0.jpg";
+  #   hash = "sha256-BOy35x/9AGHCkY0fLTrwPTsyAvctwYUkrjDvzhLZzbM=";
+  #   curlOptsList = [
+  #     "-e"
+  #     "https://www.pixiv.net/"
+  #   ];
+  # };
 in
 {
   dconf.settings = {
