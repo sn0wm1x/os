f73403b8a24dc89aa8fbaa3c40e584f1d806f676
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-25 16:42:45 +0800

    fix: remove broken librewolf

--- home/shared/features/desktop/browser/default.nix
+++ home/shared/features/desktop/browser/default.nix
@@ -1,7 +1,9 @@
 {
   imports = [
     ./google-chrome.nix
-    ./librewolf.nix
+    # TODO: enable librewolf
+    # https://github.com/NixOS/nixpkgs/pull/336304
+    # ./librewolf.nix
     ./tor-browser.nix
   ];
 }
