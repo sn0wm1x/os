40aaac9b1660e47f7476cb828d60929ef63ebe35
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-26 23:35:12 +0800

    fix(gnome): remove gnome scope

--- hosts/shared/features/desktop/gnome/default.nix
+++ hosts/shared/features/desktop/gnome/default.nix
@@ -8,32 +8,29 @@
   services.xserver.excludePackages = with pkgs; [ xterm ];
 
   # https://nixos.wiki/wiki/GNOME#Excluding_some_GNOME_applications_from_the_default_install
-  environment.gnome.excludePackages =
-    (with pkgs; [
-      # gnome-photos
-      gnome-music
-      gnome-tour
-      cheese # webcam tool
-      epiphany # web browser
-      geary # email reader
-      totem # video player
-      # ])
-      # ++ (with pkgs.gnome; [
-      # gnome-terminal
-      # gedit # text editor
-      # evince # document viewer
-      # gnome-characters
-      # tali # poker game
-      # iagno # go game
-      # hitori # sudoku game
-      # atomix # puzzle game
-    ]);
+  environment.gnome.excludePackages = with pkgs; [
+    # gnome-photos
+    gnome-music
+    gnome-tour
+    cheese # webcam tool
+    epiphany # web browser
+    geary # email reader
+    totem # video player
+    # ])
+    # ++ (with pkgs.gnome; [
+    # gnome-terminal
+    # gedit # text editor
+    # evince # document viewer
+    # gnome-characters
+    # tali # poker game
+    # iagno # go game
+    # hitori # sudoku game
+    # atomix # puzzle game
+  ];
 
-  environment.systemPackages = (with pkgs.gnome; [
-    gnome-shell-extensions
-  ])
-  ++ (with pkgs; [
+  environment.systemPackages = with pkgs; [
     dconf-editor
+    gnome-shell-extensions
     gnome-tweaks
-  ]);
+  ];
 }
