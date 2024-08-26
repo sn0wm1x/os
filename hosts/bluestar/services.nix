e49b78d6e2bfa5e6eca4a6a8afe3c1d99a85096e
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-26 23:19:06 +0800

    feat(bluestar): add ollama service

---
+++ hosts/bluestar/services.nix
@@ -0,0 +1,8 @@
+{ inputs, pkgs, ... }: {
+  services.ollama = {
+    enable = true;
+    package = inputs.sn0wm1x.packages.${pkgs.system}.ollama-sycl-bin;
+    # sudo chown -R ollama:ollama /opt/ollama
+    home = "/opt/ollama";
+  };
+}
