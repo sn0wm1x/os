1759dafba5516f7b5e586c26abc58c454bfd1b8f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-27 14:20:52 +0800

    fix(bluestar/ollama): update environments

--- hosts/bluestar/services.nix
+++ hosts/bluestar/services.nix
@@ -2,12 +2,16 @@
   services.ollama = {
     enable = true;
     package = inputs.sn0wm1x.packages.${pkgs.system}.ollama-sycl-bin;
+    # sudo mkdir /opt/ollama
     # sudo chown -R ollama:ollama /opt/ollama
     home = "/opt/ollama";
     user = "ollama";
     # group = "ollama";
     environmentVariables = {
+      OLLAMA_DEBUG = "1";
       OLLAMA_INTEL_GPU = "1";
+      # sudo mkdir /opt/ollama/tmp
+      OLLAMA_TMPDIR = "/opt/ollama/tmp/";
     };
   };
 }
