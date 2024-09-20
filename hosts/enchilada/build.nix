21e7a3c7170f0ebb7e8851d7d132adba4e70f575
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-09-20 18:45:19 +0800

    fix(enchilada/build): fix host platform

--- hosts/enchilada/build.nix
+++ hosts/enchilada/build.nix
@@ -3,6 +3,6 @@
 
   # https://nixos.wiki/wiki/NixOS_on_ARM#Build_your_own_image_natively
   nixpkgs.config.allowUnsupportedSystem = true;
-  nixpkgs.hostPlatform.system = "armv7l-linux";
+  nixpkgs.hostPlatform.system = "aarch64-linux";
   nixpkgs.buildPlatform.system = "x86_64-linux";
 }
