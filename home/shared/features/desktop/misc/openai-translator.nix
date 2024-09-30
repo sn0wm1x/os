edb8d1de6f0137c3c83e122741216a085c83151e
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-09-30 22:36:17 +0800

    chore: add openai-translator config

--- home/shared/features/desktop/misc/openai-translator.nix
+++ home/shared/features/desktop/misc/openai-translator.nix
@@ -1,3 +1,14 @@
 { outputs, pkgs, ... }: {
   home.packages = with outputs.packages.${pkgs.system}; [ openai-translator-bin ];
+
+  xdg.configFile."xyz.yetone.apps.openai-translator/config.json".text = builtins.toJSON
+    {
+      i18n = "zh-Hans";
+      ollamaAPIModel = "__custom__";
+      ollamaAPIURL = "http://127.0.0.1:11434";
+      ollamaCustomModelName = "gemma2";
+      ollamaModelLifetimeInMemory = "5m";
+      provider = "Ollama";
+      themeType = "followTheSystem";
+    };
 }
