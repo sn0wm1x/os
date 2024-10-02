81d56b3f0fff6f288a382381b3d5183ba9267ef6
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-10-02 13:14:23 +0800

    fix(gnome-mobile/overlay): fix version

--- hosts/enchilada/gnome-mobile/overlay.nix
+++ hosts/enchilada/gnome-mobile/overlay.nix
@@ -8,17 +8,18 @@ let
     rev = "5f9768a2eac29c1ed56f1fbb449a77a3523683b6";
     hash = "sha256-gdgTnxzH8BeYQAsvv++Yq/8wHi7ISk2LTBfU8hk12NM=";
   };
-  version = "46-mobile.1";
+  version = "46.1";
+  rev = "46-mobile.1";
 in
 
 {
   gnome-shell = super.gnome-shell.overrideAttrs (old: {
     inherit version;
     src = super.fetchFromGitLab {
+      inherit rev;
       domain = "gitlab.gnome.org";
       owner = "verdre";
       repo = "gnome-shell-mobile";
-      rev = version;
       hash = "sha256-NL1/mddfaL1rMidsbtV4kG2SlAZZNuR8KmqTmEE4IAM=";
       fetchSubmodules = true;
     };
@@ -36,10 +37,10 @@ in
   mutter = super.mutter.overrideAttrs (old: {
     inherit version;
     src = super.fetchFromGitLab {
+      inherit rev;
       domain = "gitlab.gnome.org";
       owner = "verdre";
       repo = "mutter-mobile";
-      rev = version;
       hash = "sha256-Xmoq//Igaz1oVt2/aLV+9WjZzW1g6yLADqg97wD3Lug=";
     };
   });
