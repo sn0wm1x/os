4ecdd0b6b287836eeac0fc018305becf63debdb7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-10-02 12:54:02 +0800

    chore(openai-translator): update config

--- home/shared/features/desktop/misc/openai-translator.nix
+++ home/shared/features/desktop/misc/openai-translator.nix
@@ -1,14 +1,18 @@
-{ outputs, pkgs, ... }: {
+{ outputs, pkgs, ... }:
+{
   home.packages = with outputs.packages.${pkgs.system}; [ openai-translator-bin ];
 
-  xdg.configFile."xyz.yetone.apps.openai-translator/config.json".text = builtins.toJSON
-    {
-      i18n = "zh-Hans";
-      ollamaAPIModel = "__custom__";
-      ollamaAPIURL = "http://127.0.0.1:11434";
-      ollamaCustomModelName = "gemma2";
-      ollamaModelLifetimeInMemory = "5m";
-      provider = "Ollama";
-      themeType = "followTheSystem";
-    };
+  # https://github.com/openai-translator/openai-translator/blob/95f92ff12e0058857146d5e700658216496d657b/src/common/utils.ts#L117-L255
+  # https://github.com/openai-translator/openai-translator/blob/95f92ff12e0058857146d5e700658216496d657b/src/common/types.ts#L52-L145
+  xdg.configFile."xyz.yetone.apps.openai-translator/config.json".text = builtins.toJSON {
+    automaticCheckForUpdates = false;
+    hideTheIconInTheDock = false;
+    i18n = "zh-Hans";
+    languageDetectionEngine = "local";
+    ollamaAPIModel = "__custom__";
+    ollamaAPIURL = "http://127.0.0.1:11434";
+    ollamaCustomModelName = "gemma2";
+    ollamaModelLifetimeInMemory = "5m";
+    provider = "Ollama";
+  };
 }
