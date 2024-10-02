19b72b2717f352e72ed3edb1379443f326092dd9
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-10-02 13:49:26 +0800

    mobile-config-firefox: fix config

--- home/shared/features/mobile/browser/firefox.nix
+++ home/shared/features/mobile/browser/firefox.nix
@@ -17,101 +17,16 @@ in
   programs.firefox = {
     enable = true;
     package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
+      autoConfigFiles = [ "${mobile-config-firefox}/src/mobile-config-autoconfig.js" ];
       extraPoliciesFiles = [ "${mobile-config-firefox}/src/policies.json" ];
-      extraPrefs = ''
-        // Copyright 2022 Arnaud Ferraris, Oliver Smith
-        // SPDX-License-Identifier: MPL-2.0
+      extraPrefsFiles = [ "${mobile-config-firefox}/src/mobile-config-prefs.js" ];
+    };
 
-        // This is a Firefox autoconfig file:
-        // https://support.mozilla.org/en-US/kb/customizing-firefox-using-autoconfig
-
-        // Import custom userChrome.css on startup or new profile creation
-        const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
-        Cu.import("resource://gre/modules/Services.jsm");
-        Cu.import("resource://gre/modules/FileUtils.jsm");
-
-        var updated = false;
-
-        // Create <profile>/chrome/ directory if not already present
-        var chromeDir = Services.dirsvc.get("ProfD", Ci.nsIFile);
-        chromeDir.append("chrome");
-        if (!chromeDir.exists()) {
-            chromeDir.create(Ci.nsIFile.DIRECTORY_TYPE, FileUtils.PERMS_DIRECTORY);
-        }
-
-        // Create nsIFile objects for userChrome.css in <profile>/chrome/ and in /etc/
-        var chromeFile = chromeDir.clone();
-        chromeFile.append("userChrome.css");
-        var defaultChrome = new FileUtils.File("${userChrome}");
-
-        // No auto-upgrade. Should this be replaced with symlinking?
-        // // Remove the existing userChrome.css if older than the installed one
-        // if (chromeFile.exists() && defaultChrome.exists() &&
-        //   chromeFile.lastModifiedTime < defaultChrome.lastModifiedTime) {
-        //   chromeFile.remove(false);
-        // }
-
-        // Copy userChrome.css to <profile>/chrome/
-        if (!chromeFile.exists()) {
-          defaultChrome.copyTo(chromeDir, "userChrome.css");
-          updated = true;
-        }
-
-        // Create nsIFile objects for userContent.css in <profile>/chrome/ and in /etc/
-        var contentFile = chromeDir.clone();
-        contentFile.append("userContent.css");
-        var defaultContent = new FileUtils.File("${userContent}");
-
-        // No auto-upgrade. Should this be replaced with symlinking?
-        // // Remove the existing userContent.css if older than the installed one
-        // if (contentFile.exists() && defaultContent.exists() &&
-        //   contentFile.lastModifiedTime < defaultContent.lastModifiedTime) {
-        //   contentFile.remove(false);
-        // }
-
-        // Copy userContent.css to <profile>/chrome/
-        if (!contentFile.exists()) {
-          defaultContent.copyTo(chromeDir, "userContent.css");
-          updated = true;
-        }
-
-        // Restart Firefox immediately if one of the files got updated
-        if (updated === true) {
-            var appStartup = Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup);
-            appStartup.quit(Ci.nsIAppStartup.eForceQuit | Ci.nsIAppStartup.eRestart);
-        }
-
-        defaultPref('general.useragent.override', 'Mozilla/5.0 (Android 11; Mobile; rv:96.0) Gecko/96.0 Firefox/96.0');
-        defaultPref('browser.urlbar.suggest.topsites', false);
-        defaultPref('browser.urlbar.suggest.engines', false);
-        defaultPref('browser.newtabpage.enabled', true);
-
-        // Enable android-style pinch-to-zoom
-        pref('dom.w3c.touch_events.enabled', true);
-        pref('apz.allow_zooming', true);
-        pref('apz.allow_double_tap_zooming', true);
-
-        // Save vertical space by hiding the titlebar
-        pref('browser.tabs.inTitlebar', 1);
-
-        // Disable search suggestions
-        pref('browser.search.suggest.enabled', false);
-
-        // Empty new tab page: faster, less distractions
-        pref('browser.newtabpage.enabled', false);
-
-        // Allow UI customizations with userChrome.css and userContent.css
-        pref('toolkit.legacyUserProfileCustomizations.stylesheets', true);
-
-        // Select the entire URL with one click
-        pref('browser.urlbar.clickSelectsAll', true);
-
-        // Disable cosmetic animations, save CPU
-        pref('toolkit.cosmeticAnimations.enabled', false);
-
-        // Disable download animations, save CPU
-        pref('browser.download.animateNotifications', false);
-      '';
+    profiles.kwa = {
+      isDefault = true;
+      id = 0;
+      name = "藍";
+      inherit userChrome userContent;
     };
 
     # https://github.com/nix-community/home-manager/pull/5195
