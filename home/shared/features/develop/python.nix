de9a8c193e6946aeb45cb73321e1bbe706713952
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-10-23 21:08:52 +0800

    chore: add huggingface-hub

--- home/shared/features/develop/python.nix
+++ home/shared/features/develop/python.nix
@@ -1,13 +1,22 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   home.packages = with pkgs; [
     (python3.withPackages (
       python3Packages: with python3Packages; [
         pipx
         pip
+        huggingface-hub
+        # TODO: check CUDA
+        # torchWithCuda
+        # torchvision
+        # torchaudio
       ]
     ))
     uv # package manager
     ruff # linter
+  ];
+
+  home.persistence."/persist${config.home.homeDirectory}".directories = [
+    ".cache/huggingface"
   ];
 }
