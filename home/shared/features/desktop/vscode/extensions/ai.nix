fd7bf46915f05671feb958149c1d0da89863405d
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-10-24 22:03:53 +0800

    chore(vscode/ai): use mistral-nemo

--- home/shared/features/desktop/vscode/extensions/ai.nix
+++ home/shared/features/desktop/vscode/extensions/ai.nix
@@ -1,37 +1,59 @@
 { osConfig, pkgs, ... }:
-let host = osConfig.networking.hostName;
-in {
+let
+  host = osConfig.networking.hostName;
+in
+{
   programs.vscode = {
     extensions =
-      if host == "bluestar" then [
-        pkgs.vscode-extensions.continue.continue
-      ] else [ ];
+      if host == "bluestar" then
+        [
+          pkgs.vscode-extensions.continue.continue
+        ]
+      else
+        [ ];
     userSettings =
-      if host == "bluestar" then {
-        "continue.telemetryEnabled" = false;
-      } else { };
+      if host == "bluestar" then
+        {
+          "continue.telemetryEnabled" = false;
+        }
+      else
+        { };
   };
 
   home.file =
-    if host == "bluestar" then {
-      ".continue/config.json".text = builtins.toJSON
-        {
+    if host == "bluestar" then
+      {
+        ".continue/config.json".text = builtins.toJSON {
+          # models = [
+          #   {
+          #     title = "Llama 3.2 3B";
+          #     provider = "ollama";
+          #     model = "llama3.2";
+          #   }
+          # ];
+          # tabAutocompleteModel = {
+          #   title = "StarCoder2 3B";
+          #   provider = "ollama";
+          #   model = "starcoder2";
+          # };
           models = [
             {
-              title = "Llama 3.2 3B";
+              title = "Mistral NeMo";
               provider = "ollama";
-              model = "llama3.2";
+              model = "mistral-nemo";
             }
           ];
           tabAutocompleteModel = {
-            title = "StarCoder2 3B";
+            title = "Mistral NeMo";
             provider = "ollama";
-            model = "starcoder2";
+            model = "mistral-nemo";
           };
           embeddingsProvider = {
             provider = "ollama";
             model = "nomic-embed-text";
           };
         };
-    } else { };
+      }
+    else
+      { };
 }
