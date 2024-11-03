5cd3d8b6cb33db257f43a1b71af6aa9a98ac8569
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-03 23:03:19 +0800

    fix(gnome): update mutter triple-buffering version

--- overlays/gnome/default.nix
+++ overlays/gnome/default.nix
@@ -1,30 +1,12 @@
 final: prev: {
-  gnome = prev.gnome.overrideScope (
-    gnomeFinal: gnomePrev: {
-      mutter = gnomePrev.mutter.overrideAttrs (old: {
-        # https://nixos.wiki/wiki/GNOME#Dynamic_triple_buffering
-        src = prev.pkgs.fetchFromGitLab {
-          domain = "gitlab.gnome.org";
-          owner = "vanvugt";
-          repo = "mutter";
-          rev = "triple-buffering-v4-46";
-          hash = "sha256-C2VfW3ThPEZ37YkX7ejlyumLnWa9oij333d5c4yfZxc=";
-        };
-
-        # https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=mutter-text-input-v1
-        # patches = [
-        #   # grab.patch
-        #   (prev.fetchpatch {
-        #     url = "https://gitlab.gnome.org/GNOME/mutter/-/commit/a99e139a68bd4e5350033163dfd85b6ce6da92a2.patch";
-        #     hash = "sha256-QyMRJ8QNxEj/WNfQM2TzZ5vq0xg33hlaE8UgjHM25Zs=";
-        #   })
-        #   # text-input-v1.patch
-        #   (prev.fetchpatch {
-        #     url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3751/diffs.patch";
-        #     hash = "sha256-KOpYon8Ajzn60voOAnj6wqXxZ7nnnJ9RIPhjGBKS5uY=";
-        #   })
-        # ];
-      });
-    }
-  );
+  mutter = prev.mutter.overrideAttrs (old: {
+    # https://nixos.wiki/wiki/GNOME#Dynamic_triple_buffering
+    src = prev.pkgs.fetchFromGitLab {
+      domain = "gitlab.gnome.org";
+      owner = "vanvugt";
+      repo = "mutter";
+      rev = "triple-buffering-v4-47";
+      hash = "sha256-JaqJvbuIAFDKJ3y/8j/7hZ+/Eqru+Mm1d3EvjfmCcug=";
+    };
+  });
 }
