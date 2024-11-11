a96b4761cdd0ad44dec1414ffdcc62f62b5d80b1
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-11 17:21:49 +0800

    fix(gnome): disable mutter overlay

--- overlays/gnome/default.nix
+++ overlays/gnome/default.nix
@@ -1,12 +1,12 @@
 final: prev: {
-  mutter = prev.mutter.overrideAttrs (old: {
-    # https://nixos.wiki/wiki/GNOME#Dynamic_triple_buffering
-    src = prev.pkgs.fetchFromGitLab {
-      domain = "gitlab.gnome.org";
-      owner = "vanvugt";
-      repo = "mutter";
-      rev = "triple-buffering-v4-47";
-      hash = "sha256-JaqJvbuIAFDKJ3y/8j/7hZ+/Eqru+Mm1d3EvjfmCcug=";
-    };
-  });
+  # mutter = prev.mutter.overrideAttrs (old: {
+  #   # https://nixos.wiki/wiki/GNOME#Dynamic_triple_buffering
+  #   src = prev.pkgs.fetchFromGitLab {
+  #     domain = "gitlab.gnome.org";
+  #     owner = "vanvugt";
+  #     repo = "mutter";
+  #     rev = "triple-buffering-v4-47";
+  #     hash = "sha256-JaqJvbuIAFDKJ3y/8j/7hZ+/Eqru+Mm1d3EvjfmCcug=";
+  #   };
+  # });
 }
