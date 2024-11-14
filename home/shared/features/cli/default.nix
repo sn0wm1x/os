6ae73d6dd720dcf5f394919a9695334cb6afcbe7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-14 23:06:54 +0800

    chore: add tealdeer, procs, httpie

--- home/shared/features/cli/default.nix
+++ home/shared/features/cli/default.nix
@@ -9,11 +9,14 @@
     ./git.nix
     ./helix.nix
     ./just.nix
+    ./tealdeer.nix
     ./zellij.nix
   ];
   home.packages = with pkgs; [
     # eza # Better ls
     ripgrep # Better grep
     fd # Better find
+    procs # Better ps
+    httpie # HTTP client
   ];
 }
---
+++ home/shared/features/cli/tealdeer.nix
@@ -0,0 +1,3 @@
+{
+  programs.tealdeer.enable = true;
+}
