ee96996cf74828be61394691dc582a4eeeb14a5c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-12-23 23:55:20 +0800

    refactor: setup xdg desktop portal

--- hosts/shared/features/desktop/common/default.nix
+++ hosts/shared/features/desktop/common/default.nix
@@ -3,5 +3,6 @@
     ./fcitx5.nix
     ./pipewire.nix
     ./wayland.nix
+    ./xdg-portal.nix
   ];
 }
---
+++ hosts/shared/features/desktop/common/xdg-portal.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+{
+  xdg.portal = {
+    enable = true;
+    # xdgOpenUsePortal = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk
+    ];
+  };
+}
