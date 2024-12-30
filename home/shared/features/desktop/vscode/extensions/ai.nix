cf5d1797c0cd5337848b35791f319306bc90a214
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-12-30 15:47:20 +0800

    chore(ai): use 1.5b model instead of 3b

--- home/shared/features/desktop/vscode/extensions/ai.nix
+++ home/shared/features/desktop/vscode/extensions/ai.nix
@@ -11,21 +11,16 @@
 
   home.file.".continue/config.json".text = builtins.toJSON {
     models = [
-      # {
-      #   title = "Qwen 2.5 3B";
-      #   provider = "ollama";
-      #   model = "qwen2.5:3b";
-      # }
       {
-        title = "Qwen 2.5 Coder 3B";
+        title = "Qwen 2.5 Coder 1.5B";
         provider = "ollama";
-        model = "qwen2.5-coder:3b";
+        model = "qwen2.5-coder:1.5b";
       }
     ];
     tabAutocompleteModel = {
-      title = "Qwen 2.5 Coder 3B";
+      title = "Qwen 2.5 Coder 1.5B";
       provider = "ollama";
-      model = "qwen2.5-coder:3b";
+      model = "qwen2.5-coder:1.5b";
     };
     embeddingsProvider = {
       provider = "ollama";
