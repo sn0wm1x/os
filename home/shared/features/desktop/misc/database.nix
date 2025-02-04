64f058160c0af0b632fcd8a34cfb2fe2e1db2009
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-04 22:02:53 +0800

    chore(database): add antares

--- home/shared/features/desktop/misc/database.nix
+++ home/shared/features/desktop/misc/database.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    # antares # https://github.com/antares-sql/antares
+    antares # https://github.com/antares-sql/antares
     sqlitebrowser # https://github.com/sqlitebrowser/sqlitebrowser
   ];
 }
