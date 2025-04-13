dba6ac41e1337616fa4ae2bbb20244efc4b28f45
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-04-13 15:55:21 +0800

    chore: update dconf

--- home/shared/features/desktop/gnome/dconf.nix
+++ home/shared/features/desktop/gnome/dconf.nix
@@ -1,21 +1,26 @@
 {
-  dconf.settings."org/gnome/mutter" = {
-    # Enable Dynamic Workspaces
-    dynamic-workspaces = true;
-    # Enable VRR & Fractional scaling
-    experimental-features = [
-      "variable-refresh-rate"
-      "scale-monitor-framebuffer"
-      "xwayland-native-scaling"
-    ];
-  };
-
-  dconf.settings."org/gnome/desktop/interface" = {
-    color-scheme = "prefer-dark";
-    font-antialiasing = "rgba";
-    font-hinting = "slight";
-    font-name = "Noto Sans CJK SC 12";
-    document-font-name = "Noto Sans CJK SC 12";
-    monospace-font-name = "MonaspiceNe Nerd Font 10";
+  dconf.settings = {
+    "org/gnome/mutter" = {
+      # Enable Dynamic Workspaces
+      dynamic-workspaces = true;
+      # Enable VRR & Fractional scaling
+      experimental-features = [
+        "variable-refresh-rate"
+        "scale-monitor-framebuffer"
+        "xwayland-native-scaling"
+      ];
+    };
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      font-antialiasing = "rgba";
+      font-hinting = "slight";
+      font-name = "Noto Sans CJK SC 12";
+      document-font-name = "Noto Sans CJK SC 12";
+      monospace-font-name = "MonaspiceNe Nerd Font 10";
+    };
+    "org/gnome/settings-daemon/plugins/power" = {
+      sleep-inactive-ac-type = "nothing";
+      power-button-action = "interactive";
+    };
   };
 }
