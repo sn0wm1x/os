f071f52a14370f22d7b5eb9ae450681a49ceffee
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-05-26 14:59:43 +0800

    chore: clean ghostty settings

--- home/shared/features/desktop/misc/ghostty.nix
+++ home/shared/features/desktop/misc/ghostty.nix
@@ -4,9 +4,6 @@
     settings = {
       theme = "catppuccin-frappe";
       font-family = "MonaspiceNe NFM";
-      font-family-italic = "MonaspiceNe NFM Italic";
-      font-family-bold = "MonaspiceNe NFM Bold";
-      font-family-bold-italic = "MonaspiceNe NFM Bold Italic";
     };
   };
 }
