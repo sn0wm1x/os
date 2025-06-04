17be3e4bd645549dc7fadfc8ec79a99fc0c713ee
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-06-04 19:36:02 +0800

    fix(akahitoha/ollama): remove acceleration

--- hosts/akahitoha/services.nix
+++ hosts/akahitoha/services.nix
@@ -1,10 +1,10 @@
 {
   services.ollama = {
     enable = true;
-    acceleration = "rocm";
+    # acceleration = "rocm";
     # https://github.com/ollama/ollama/pull/5426
     # https://github.com/ollama/ollama/pull/6282
-    rocmOverrideGfx = "11.0.2";
+    # rocmOverrideGfx = "11.0.2";
     # sudo chown -R ollama:ollama /opt/ollama
     home = "/opt/ollama";
     user = "ollama";
