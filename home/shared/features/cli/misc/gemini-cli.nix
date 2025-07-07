3330fcdc866b36b320370b24591a27c8ccdffab5
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-07-07 16:35:41 +0800

    chore: persist gemini-cli config

--- home/shared/features/cli/misc/gemini-cli.nix
+++ home/shared/features/cli/misc/gemini-cli.nix
@@ -1,4 +1,14 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   home.packages = with pkgs; [ gemini-cli ];
+
+  home.file.".gemini/settings.json".text = builtins.toJSON {
+    theme = "ANSI";
+    selectedAuthType = "oauth-personal";
+  };
+
+  home.persistence."/persist${config.home.homeDirectory}".files = [
+    ".gemini/oauth_creds.json"
+    ".gemini/user_id"
+  ];
 }
