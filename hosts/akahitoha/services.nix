acd5262dcb0cfb2b46671875a59f7fa4f35516cd
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-07-07 15:51:34 +0800

    fix(akahitoha): disable ollama acceleration

--- hosts/akahitoha/services.nix
+++ hosts/akahitoha/services.nix
@@ -1,6 +1,7 @@
 {
   services.ollama = {
     enable = true;
+    acceleration = false;
     # acceleration = "rocm";
     # https://github.com/ollama/ollama/pull/5426
     # https://github.com/ollama/ollama/pull/6282
