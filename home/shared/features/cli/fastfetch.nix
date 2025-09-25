6614d61725658283f2525f3f70fa8535aad05ea7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-09-25 14:52:51 +0800

    chore: update fastfetch config

--- home/shared/features/cli/fastfetch.nix
+++ home/shared/features/cli/fastfetch.nix
@@ -11,46 +11,108 @@ in
     settings = {
       # "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
       logo = {
-        type = "kitty-direct"; # TODO: remove this
+        type = "kitty-direct";
         source = "${logo}";
+        padding = {
+          top = 4;
+          right = 2;
+          left = 2;
+        };
       };
-      # https://github.com/fastfetch-cli/fastfetch/blob/dev/presets/screenfetch.jsonc
       modules = [
-        "title"
+        "break"
         {
-          "type" = "os";
-          "format" = "SN0WM1X (Nix) OS ({11}, {12})";
-        }
-        "kernel"
-        "uptime"
-        {
-          "type" = "packages";
-          "format" = "{all}";
-        }
-        "shell"
-        {
-          "type" = "display";
-          "key" = "Resolution";
-          "compactType" = "original";
-        }
-        "de"
-        "wm"
-        "wmtheme"
-        {
-          "type" = "terminalfont";
-          "key" = "font";
+          "type" = "custom";
+          "format" = "\u001b[90m┌──────────────────────Hardware──────────────────────┐";
         }
         {
-          "type" = "disk";
-          "folders" = "/";
-          "key" = "Disk";
+          type = "host";
+          key = "├{icon}";
+          keyColor = "blue";
         }
-        "cpu"
-        "gpu"
         {
-          "type" = "memory";
-          "key" = "RAM";
+          type = "cpu";
+          key = "├{icon}";
+          showPeCoreCount = true;
+          keyColor = "blue";
         }
+        {
+          type = "gpu";
+          key = "├{icon}";
+          detectionMethod = "pci";
+          keyColor = "blue";
+        }
+        {
+          type = "display";
+          key = "├{icon}";
+          keyColor = "blue";
+        }
+        {
+          type = "disk";
+          folders = "/";
+          key = "├{icon}";
+          keyColor = "blue";
+        }
+        {
+          type = "memory";
+          key = "├{icon}";
+          keyColor = "blue";
+        }
+        {
+          type = "custom";
+          format = "\u001b[90m└────────────────────────────────────────────────────┘";
+        }
+        "break"
+        {
+          type = "custom";
+          format = "\u001b[90m┌──────────────────────Software──────────────────────┐";
+        }
+        {
+          type = "os";
+          format = "SN0WM1X (Nix) OS ({11}, {12})";
+          key = "├󱄅";
+          keyColor = "magenta";
+        }
+        {
+          type = "kernel";
+          key = "├";
+          keyColor = "magenta";
+        }
+        {
+          type = "packages";
+          key = "├{icon}";
+          keyColor = "magenta";
+        }
+        {
+          type = "shell";
+          key = "├{icon}";
+          keyColor = "magenta";
+        }
+        {
+          type = "de";
+          key = "├{icon}";
+          keyColor = "magenta";
+        }
+        {
+          type = "wm";
+          key = "├{icon}";
+          keyColor = "magenta";
+        }
+        {
+          type = "wmtheme";
+          key = "├{icon}";
+          keyColor = "magenta";
+        }
+        {
+          type = "uptime";
+          key = "├{icon}";
+          keyColor = "magenta";
+        }
+        {
+          type = "custom";
+          format = "\u001b[90m└────────────────────────────────────────────────────┘";
+        }
+        "break"
       ];
     };
   };
