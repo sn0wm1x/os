3bc0c6a29294ff569fbc6e58ff7c3b9ddb764944
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-09-29 15:44:30 +0800

    fix: fastfetch config

--- home/shared/features/cli/fastfetch.nix
+++ home/shared/features/cli/fastfetch.nix
@@ -4,11 +4,12 @@ let
     url = "https://github.com/sn0wm1x.png";
     hash = "sha256-7gAf++UN5o7+aP8jLno46zaEtni7vwNBrxD1pDkcA3A=";
   };
+  json = builtins.fromJSON (builtins.readFile ./fastfetch.json);
 in
 {
   programs.fastfetch = {
     enable = true;
-    settings = {
+    settings = json // {
       # "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
       logo = {
         type = "kitty-direct";
@@ -19,106 +20,12 @@ in
           left = 2;
         };
       };
-      modules = [
-        "break"
-        {
-          "type" = "custom";
-          "format" = "\\u001b[90m┌──────────────────────Hardware──────────────────────┐";
-        }
-        {
-          type = "host";
-          key = "├{icon}";
-          keyColor = "blue";
-        }
-        {
-          type = "cpu";
-          key = "├{icon}";
-          showPeCoreCount = true;
-          keyColor = "blue";
-        }
-        {
-          type = "gpu";
-          key = "├{icon}";
-          detectionMethod = "pci";
-          keyColor = "blue";
-        }
-        {
-          type = "display";
-          key = "├{icon}";
-          keyColor = "blue";
-        }
-        {
-          type = "disk";
-          folders = "/";
-          key = "├{icon}";
-          keyColor = "blue";
-        }
-        {
-          type = "memory";
-          key = "├{icon}";
-          keyColor = "blue";
-        }
-        {
-          type = "custom";
-          format = "\\u001b[90m└────────────────────────────────────────────────────┘";
-        }
-        "break"
-        {
-          type = "custom";
-          format = "\\u001b[90m┌──────────────────────Software──────────────────────┐";
-        }
-        {
-          type = "os";
-          format = "SN0WM1X (Nix) OS ({11}, {12})";
-          key = "├󱄅";
-          keyColor = "magenta";
-        }
-        {
-          type = "kernel";
-          key = "├";
-          keyColor = "magenta";
-        }
-        {
-          type = "packages";
-          key = "├{icon}";
-          keyColor = "magenta";
-        }
-        {
-          type = "shell";
-          key = "├{icon}";
-          keyColor = "magenta";
-        }
-        {
-          type = "de";
-          key = "├{icon}";
-          keyColor = "magenta";
-        }
-        {
-          type = "wm";
-          key = "├{icon}";
-          keyColor = "magenta";
-        }
-        {
-          type = "wmtheme";
-          key = "├{icon}";
-          keyColor = "magenta";
-        }
-        {
-          type = "uptime";
-          key = "├{icon}";
-          keyColor = "magenta";
-        }
-        {
-          type = "custom";
-          format = "\\u001b[90m└────────────────────────────────────────────────────┘";
-        }
-        "break"
-      ];
     };
   };
 
   home.shellAliases = {
     nf = "fastfetch";
     neofetch = "fastfetch";
+    screenfetch = "fastfetch";
   };
 }
