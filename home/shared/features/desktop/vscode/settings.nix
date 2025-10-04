4b8fa523e0cd157e1441a8e126f9ad68d945757b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-10-04 20:38:10 +0800

    chore(vscode): disable workspace trust feature

--- home/shared/features/desktop/vscode/settings.nix
+++ home/shared/features/desktop/vscode/settings.nix
@@ -26,14 +26,13 @@ let
 in
 {
   programs.vscode.profiles.default.userSettings = {
+    "security.workspace.trust.enabled" = false;
     "extensions.autoCheckUpdates" = false;
     "extensions.ignoreRecommendations" = true;
     "update.mode" = "none";
     # https://gist.github.com/hyperupcall/99e355405611be6c4e0a38b6e3e8aad0
     "telemetry.telemetryLevel" = "off";
     # Extensions
-    "gitlens.showWelcomeOnInstall" = false;
-    "gitlens.showWhatsNewAfterUpgrades" = false;
     "redhat.telemetry.enabled" = false;
   }
   // editor
