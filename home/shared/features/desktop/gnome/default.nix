495ee051dbcadf28ee822b962e21469083217d37
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-12-09 13:44:53 +0800

    chore: persist gnome keyring

--- home/shared/features/desktop/gnome/default.nix
+++ home/shared/features/desktop/gnome/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, config, osConfig, ... }:
 {
   imports = [
     ./circle.nix
@@ -7,5 +7,10 @@
     ./gtk.nix
     ./ptyxis.nix
     ./wallpaper.nix
+  ];
+
+  # gnome-keyring
+  home.persistence."/persist${config.home.homeDirectory}".directories = lib.mkIf osConfig.services.gnome.gnome-keyring.enable [
+    ".local/share/keyrings"
   ];
 }
