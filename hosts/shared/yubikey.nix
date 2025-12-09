f925026bd0b5f73491077a4f476e3d9675ddf8c6
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-12-09 13:36:16 +0800

    chore: remove yubikey udev rules

--- hosts/shared/yubikey.nix
+++ hosts/shared/yubikey.nix
@@ -1,27 +1,27 @@
 { pkgs, ... }:
 {
-  # https://nixos.wiki/wiki/Yubikey#GPG_and_SSH
+  # https://wiki.nixos.org/wiki/Yubikey#GPG_and_SSH
   programs.gnupg.agent.enable = true;
   programs.gnupg.agent.enableSSHSupport = true;
   environment.systemPackages = with pkgs; [ yubikey-personalization ];
   services.udev.packages = with pkgs; [ yubikey-personalization ];
 
-  # https://nixos.wiki/wiki/Yubikey#Smartcard_mode
+  # https://wiki.nixos.org/wiki/Yubikey#Smartcard_mode
   services.pcscd.enable = true;
 
-  # https://nixos.wiki/wiki/Yubikey#pam_u2f
+  # https://wiki.nixos.org/wiki/Yubikey#pam_u2f
   security.pam.u2f.enable = true;
   # security.pam.u2f.control = "required";
   security.pam.services.login.u2fAuth = true;
   security.pam.services.sudo.u2fAuth = true;
 
-  # https://nixos.wiki/wiki/Yubikey#Locking_the_screen_when_a_Yubikey_is_unplugged
-  services.udev.extraRules = ''
-    ACTION=="remove",\
-    ENV{ID_BUS}=="usb",\
-    ENV{ID_MODEL_ID}=="0407",\
-    ENV{ID_VENDOR_ID}=="1050",\
-    ENV{ID_VENDOR}=="Yubico",\
-    RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
-  '';
+  # https://wiki.nixos.org/wiki/Yubikey#Locking_the_screen_when_a_Yubikey_is_unplugged
+  # services.udev.extraRules = ''
+  #   ACTION=="remove",\
+  #   ENV{ID_BUS}=="usb",\
+  #   ENV{ID_MODEL_ID}=="0407",\
+  #   ENV{ID_VENDOR_ID}=="1050",\
+  #   ENV{ID_VENDOR}=="Yubico",\
+  #   RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+  # '';
 }
