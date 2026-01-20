3252925ea8ae11bb380c1aaf7ba816b8740cf71a
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-01-20 10:41:42 +0800

    chore(ollama): adjust env

--- hosts/bluestar/services.nix
+++ hosts/bluestar/services.nix
@@ -7,7 +7,7 @@
     user = "ollama";
     environmentVariables = {
       OLLAMA_ORIGINS = "*";
-      OLLAMA_CONTEXT_LENGTH = "32768";
+      OLLAMA_CONTEXT_LENGTH = "131072"; # 128K
       OLLAMA_FLASH_ATTENTION = "1";
       OLLAMA_KV_CACHE_TYPE = "q8_0";
     };
