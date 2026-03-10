1f439288a6b5a776aa6a945f383e870a778e7122
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-03-10 20:28:57 +0800

    chore(codex): clean settings, add persist

--- home/shared/features/cli/misc/codex.nix
+++ home/shared/features/cli/misc/codex.nix
@@ -2,11 +2,15 @@
   # https://home-manager-options.extranix.com/?query=codex&release=master
   programs.codex = {
     enable = true;
-    settings = {
-      model = "gpt-oss";
-      model_provider = "ollama";
-      approval_policy = "untrusted";
-      sandbox_mode = "workspace-write";
-    };
+    # settings = {
+    #   model = "gpt-oss";
+    #   model_provider = "ollama";
+    #   approval_policy = "untrusted";
+    #   sandbox_mode = "workspace-write";
+    # };
   };
+
+  home.persistence."/persist".directories = [
+    ".codex"
+  ];
 }
