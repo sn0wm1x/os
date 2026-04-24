b0d2923fe98d2728a0ea83d9373dc22c01fa6842
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-04-24 17:05:43 +0800

    fix: inshellisense

--- modules/home-manager/nushell.nix
+++ modules/home-manager/nushell.nix
@@ -69,16 +69,15 @@ with lib;
     programs.zoxide.enableNushellIntegration = true;
 
     home.packages = with pkgs; [
-      inshellisense.overrideAttrs
-      (old: {
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.makeWrapper ];
+      (inshellisense.overrideAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ makeWrapper ];
         postInstall = (old.postInstall or "") + ''
           for bin in is inshellisense; do
             wrapProgram "$out/bin/$bin" \
               --run "cd $out/lib/node_modules/@microsoft/inshellisense"
           done
         '';
-      })
+      }))
     ];
 
     home.persistence = lib.mkIf config.sn0wm1x.impermanence.enable {
