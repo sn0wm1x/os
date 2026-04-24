6c6eb793b578b97b5d5f790c3505ad50b3df2565
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-04-24 16:04:36 +0800

    chore: persist cargo data

--- home/shared/features/develop/rust.nix
+++ home/shared/features/develop/rust.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   home.packages = with pkgs; [
     # rust-toolchain
@@ -24,9 +24,16 @@
     bacon
   ];
 
-  # Certain Rust tools won't work without this
-  # https://nixos.wiki/wiki/Rust#Shell.nix_example
   home.sessionVariables = {
+    # https://wiki.archlinux.org/title/XDG_Base_Directory#Partial
+    CARGO_HOME = "${config.xdg.dataHome}/cargo";
+
+    # Certain Rust tools won't work without this
+    # https://wiki.nixos.org/wiki/Rust#Shell.nix_example
     RUST_SRC_PATH = pkgs.rust.packages.stable.rustPlatform.rustLibSrc;
   };
+
+  home.persistence."/persist".directories = [
+    ".local/share/cargo"
+  ];
 }
