0b844f9cf6fec166535b13059590f41beb501237
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-04-24 15:53:35 +0800

    chore(git): add signing format

--- home/kwa/programs/git.nix
+++ home/kwa/programs/git.nix
@@ -2,5 +2,6 @@
   programs.git.settings.user.name = "藍+85CD";
   programs.git.settings.user.email = "50108258+kwaa@users.noreply.github.com";
   programs.git.signing.key = "BCB0111111111111"; # 0x4444777733334444
+  programs.git.signing.format = "openpgp";
   programs.git.signing.signByDefault = true;
 }
