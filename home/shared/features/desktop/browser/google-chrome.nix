ac7c461a21d910db9a69f328ddeffd8bc01d835c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-08-01 12:22:03 +0800

    fix(chrome): disable vulkan

--- home/shared/features/desktop/browser/google-chrome.nix
+++ home/shared/features/desktop/browser/google-chrome.nix
@@ -14,21 +14,25 @@ let
     # force ozone wayland & fcitx5 wayland fix
     # https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland#Chromium_.2F_Electron
     "--ozone-platform=wayland --enable-wayland-ime --wayland-text-input-version=3"
-
-    # WebGPU
-    "--enable-features=Vulkan"
   ];
 
-  # google-chrome
-  googleChromeStableBase = pkgs.google-chrome.override {
+  # google-chrome / google-chrome-stable
+  googleChromeStable = pkgs.google-chrome.override {
     inherit commandLineArgs;
   };
 
-  googleChromeStable =
-    if isBluestar then
-      googleChromeStableBase.overrideAttrs (old: {
+  # google-chrome-dev / google-chrome-unstable
+  googleChromeDev =
+    (inputs.browser-previews.packages.${pkgs.stdenv.hostPlatform.system}.google-chrome-dev.override {
+      inherit commandLineArgs;
+    }).overrideAttrs
+      (old: {
+        commandLineArgs = old.commandLineArgs ++ [
+          # WebGPU
+          "--enable-features=Vulkan"
+        ];
         postFixup = (old.postFixup or "") + ''
-          wrapProgram "$out/bin/google-chrome-stable" \
+         wrapProgram "$out/bin/google-chrome-stable" \
             --unset __NV_PRIME_RENDER_OFFLOAD \
             --unset __NV_PRIME_RENDER_OFFLOAD_PROVIDER \
             --unset __GLX_VENDOR_LIBRARY_NAME \
@@ -37,26 +41,6 @@ let
             --set MESA_VK_DEVICE_SELECT '8086:a780!' \
             --set VK_DRIVER_FILES /run/opengl-driver/share/vulkan/icd.d/intel_icd.x86_64.json \
             --set __VK_LAYER_NV_optimus non_NVIDIA_only
-        '';
-      })
-    else
-      googleChromeStableBase;
-
-  # google-chrome-dev / google-chrome-unstable
-  googleChromeDev =
-    (inputs.browser-previews.packages.${pkgs.stdenv.hostPlatform.system}.google-chrome-dev.override {
-      inherit commandLineArgs;
-    }).overrideAttrs
-      (old: {
-        postFixup = (old.postFixup or "") + ''
-          wrapProgram "$out/bin/google-chrome-unstable" \
-            --set DRI_PRIME pci-0000_01_00_0 \
-            --set __EGL_VENDOR_LIBRARY_FILENAMES /run/opengl-driver/share/glvnd/egl_vendor.d/10_nvidia.json \
-            --set VK_DRIVER_FILES /run/opengl-driver/share/vulkan/icd.d/nvidia_icd.json \
-            --set __NV_PRIME_RENDER_OFFLOAD 1 \
-            --set __NV_PRIME_RENDER_OFFLOAD_PROVIDER NVIDIA-G0 \
-            --set __GLX_VENDOR_LIBRARY_NAME nvidia \
-            --set __VK_LAYER_NV_optimus NVIDIA_only
         '';
       });
 in
