b0adca9e6ddb541655d30c76c661ad53081af00e
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-09-07 20:35:42 +0800

    fix(vscode): syntax highlighting

--- home/shared/features/desktop/vscode/default.nix
+++ home/shared/features/desktop/vscode/default.nix
@@ -1,3 +1,14 @@
+{ pkgs, ... }:
+let
+  # TODO: remove hotfix
+  # https://github.com/NixOS/nixpkgs/issues/560776#issuecomment-5569887181
+  vscode = pkgs.vscode.overrideAttrs (old: {
+    postPatch = old.postPatch + ''
+      rm -rf "resources/app/node_modules.asar.unpacked"
+      ln -rs "resources/app/node_modules" "resources/app/node_modules.asar.unpacked"
+    '';
+  });
+in
 {
   imports = [
     ./extensions
@@ -5,7 +16,10 @@
     ./settings.nix
   ];
 
-  programs.vscode.enable = true;
+  programs.vscode = {
+    enable = true;
+    package = vscode;
+  };
   programs.vscode.profiles.default.enableUpdateCheck = false;
 
   # does not exist in home-manager
